c03fed4c7a293e319489e5a1c3056543b25233b6
Alejandro Navarro <alexnavarro57@gmail.com>
2026-06-01 01:41:15 -0700

    Set up Husky.

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -11,11 +11,11 @@ function App() {
    }, []);
 
    return (
-      <div className="p-4">
+<div className="p-4">
          <p className="font-bold text-3xl">{message}</p>
-         <Button>Click me</Button>
+                    <Button>Click me!</Button>
       </div>
-   );
+   )
 }
 
 export default App;
