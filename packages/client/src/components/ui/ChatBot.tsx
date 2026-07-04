dc5a498808b1c9c6447481ed61bd092e4909ff9b
Alejandro Navarro <alexnavarro57@gmail.com>
2026-07-04 01:48:13 -0700

    Added call to the backend

--- packages/client/src/components/ui/ChatBot.tsx
+++ packages/client/src/components/ui/ChatBot.tsx
@@ -1,4 +1,6 @@
 import { Button } from './button';
+import axios from 'axios';
+import { useRef } from 'react';
 import { useForm } from 'react-hook-form';
 import { FaArrowUp } from 'react-icons/fa';
 
@@ -7,11 +9,19 @@ type FormData = {
 };
 
 const ChatBot = () => {
+   const conversationId = useRef(crypto.randomUUID());
+
    const { register, handleSubmit, reset, formState } = useForm<FormData>();
 
-   const onSubmit = (data: FormData) => {
-      console.log(data);
+   const onSubmit = async ({ prompt }: FormData) => {
       reset();
+
+      const { data } = await axios.post('/api/chat', {
+         prompt,
+         conversationId: conversationId.current,
+      });
+
+      console.log(data);
    };
 
    const onKeyDown = (e: React.KeyboardEvent<HTMLFormElement>) => {
